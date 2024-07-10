756f634e800b7d2fa5d8738c145b106d13c4a20c
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-07-10 11:11:31 -0600

    updates config for better-escape

--- lua/vieko/plugins/better-escape.lua
+++ lua/vieko/plugins/better-escape.lua
@@ -2,7 +2,38 @@ return {
   "max397574/better-escape.nvim",
   config = function()
     require("better_escape").setup({
-      mapping = { "jk" },
+      timeout = vim.o.timeoutlen,
+      default_mappings = true,
+      mappings = {
+        i = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        c = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        t = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        v = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+        s = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+      },
     })
   end,
 }
