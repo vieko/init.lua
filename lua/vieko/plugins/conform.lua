d2a2f0f5fc38d3f0450bb5ce033c554f904bc45f
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-09-29 18:59:00 -0600

    ticks up timeout for conform

--- lua/vieko/plugins/conform.lua
+++ lua/vieko/plugins/conform.lua
@@ -17,7 +17,7 @@ return { -- Autoformat
     format_on_save = function(bufnr)
       local disable_filetypes = { c = true, cpp = true }
       return {
-        timeout_ms = 500,
+        timeout_ms = 5000,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
