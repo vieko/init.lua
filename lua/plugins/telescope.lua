db330e2d3d3f5c3c0d092744c112e61539e11cc5
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-12-21 11:29:33 -0700

    Adds fzf-lua and disables telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
   "nvim-telescope/telescope.nvim",
   event = "VimEnter",
   branch = "0.1.x",
+  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     {
