ed63e4f87191acccbdd37446a3b85d8244c3a44e
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-02-11 23:32:06 -0700

    updates to one dark

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,72 +1,92 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "auto",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      term_colors = true,
-      dim_inactive = {
-        enabled = true,
-      },
-      highlight_overrides = {
-        all = function(colors)
-          return {
-            CursorLineNr = { fg = colors.peach, bold = true },
-            CopilotSuggestion = { fg = colors.surface1 },
-            NeoTreeModified = { fg = colors.text },
-            NeoTreeFadeText1 = { fg = colors.surface1 },
-            NeoTreeFadeText2 = { fg = colors.surface1 },
-            NeoTreeDotfile = { fg = colors.surface1 },
-            NeoTreeDimText = { fg = colors.surface1 },
-            NeoTreeHiddenByName = { fg = colors.surface1 },
-            NeoTreeWindowsHidden = { fg = colors.surface1 },
-          }
-        end,
-      },
-      integrations = {
-        fidget = true,
-        flash = true,
-        gitsigns = true,
-        mason = true,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load()
+    end,
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    lazy = false,
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    enabled = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "auto",
+        background = {
+          light = "latte",
+          dark = "mocha",
         },
-        cmp = true,
-        native_lsp = {
+        term_colors = true,
+        dim_inactive = {
           enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
-          inlay_hints = {
-            background = true,
-          },
         },
-        treesitter = true,
-        telescope = { enabled = true, style = "classic" },
-        which_key = true,
-        lsp_trouble = false,
-      },
-    })
-  end,
-  init = function()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("catppuccin")
-    -- vim.cmd.hi 'Comment gui=none'
-  end,
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              CursorLineNr = { fg = colors.peach, bold = true },
+              CopilotSuggestion = { fg = colors.surface1 },
+              NeoTreeModified = { fg = colors.text },
+              NeoTreeFadeText1 = { fg = colors.surface1 },
+              NeoTreeFadeText2 = { fg = colors.surface1 },
+              NeoTreeDotfile = { fg = colors.surface1 },
+              NeoTreeDimText = { fg = colors.surface1 },
+              NeoTreeHiddenByName = { fg = colors.surface1 },
+              NeoTreeWindowsHidden = { fg = colors.surface1 },
+            }
+          end,
+        },
+        integrations = {
+          fidget = true,
+          flash = true,
+          gitsigns = true,
+          mason = true,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          cmp = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          treesitter = true,
+          telescope = { enabled = true, style = "classic" },
+          which_key = true,
+          lsp_trouble = false,
+        },
+      })
+    end,
+    init = function()
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("onedark")
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
