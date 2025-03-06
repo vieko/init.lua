c636381e4d1a23e5a373a6b907900a50b40663da
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-03-05 18:00:09 -0700

    I may have added this to the wrong repo

--- lua/langs/tailwind.lua
+++ lua/langs/tailwind.lua
@@ -5,7 +5,28 @@ return {
       servers = {
         tailwindcss = {
           filetypes_exclude = { "markdown" },
-          filetypes_include = {},
+          filetypes_include = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+          init_options = {
+            userLanguages = {
+              typescript = "javascript",
+              typescriptreact = "javascriptreact"
+            },
+          },
+          settings = {
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  { "cn\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                  { "clsx\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                  { "twMerge\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                },
+              },
+              includeLanguages = {
+                typescript = "javascript",
+                typescriptreact = "javascriptreact",
+              },
+            },
+          },
         },
       },
       setup = {
@@ -21,6 +42,27 @@ return {
           end, opts.filetypes)
 
           vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+          
+          -- Add root_dir pattern for monorepo
+          if not opts.root_dir then
+            opts.root_dir = function(fname)
+              local root_patterns = {
+                "tailwind.config.js",
+                "tailwind.config.ts",
+                "tailwind.config.cjs",
+                "tailwind.config.mjs",
+                "postcss.config.js",
+                "postcss.config.ts",
+                "postcss.config.cjs",
+                "postcss.config.mjs"
+              }
+              local util = require("lspconfig.util")
+              -- Add parent directories for monorepo patterns
+              return util.root_pattern(unpack(root_patterns))(fname) or
+                util.root_pattern("package.json", "tsconfig.json")(fname) or
+                util.find_git_ancestor(fname)
+            end
+          end
         end,
       },
     },
